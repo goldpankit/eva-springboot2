8014a3bd18109e9e77048d4cb9ef3789e6beadaa
Caesar Liu <ck9300001@qq.com>
2024-02-09 23:57:02 +0800

    调整配置、增加kit配置文件

--- sql/init/data.sql
+++ sql/init/data.sql
@@ -1,3 +1,4 @@
+USE eva4;
 -- ################ 用户数据 ################
 INSERT INTO `system_user` (`id`, `username`, `real_name`, `emp_no`, `birthday`, `gender`, `email`, `email_digest`, `mobile`, `mobile_digest`, `avatar`, `password`, `salt`, `created_by`, `created_at`, `updated_by`, `updated_at`, `deleted`) VALUES (1, 'admin', '伊娃', NULL, NULL, 'MALE', NULL, NULL, NULL, NULL, '/avatar/man.png', '5ca1f224ef123998d99d8738e7727253', 'kl38c6', NULL, CURRENT_TIMESTAMP, NULL, NULL, 0);
 
