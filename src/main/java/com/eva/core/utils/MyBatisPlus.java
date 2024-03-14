196053ad297aef92b3ab5be2ae44478fdd184eb3
Caesar Liu <ck9300001@qq.com>
2024-03-14 17:58:31 +0800

    调整返回参数

--- src/main/java/com/eva/core/utils/MyBatisPlus.java
+++ src/main/java/com/eva/core/utils/MyBatisPlus.java
@@ -9,12 +9,10 @@ public class MyBatisPlus {
 
     /**
      * 将空转为null，用于mybatis plus查询数据时将值为""的字段转为null，防止将空字符串作为条件。
-     *
-     * @return T
      */
-    public <T> T blankToNull(T object) {
+    public <T> void blankToNull(T object) {
         if (object == null) {
-            return null;
+            return;
         }
         try {
             Class<?> clazz = object.getClass();
@@ -26,7 +24,6 @@ public class MyBatisPlus {
                     field.set(object, null);
                 }
             }
-            return object;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
